f0fe5309c1743ecdc65174274a2fa1eabcf2de4f
Gabriel Dias <129539000+bielxrd@users.noreply.github.com>
2024-02-01 16:38:37 -0300

    Estrutura inicial criado pelo Spring Initialiazr (Backend)

---
+++ Backend/Projeto-Barbearia/src/main/java/br/com/gabrielrichard/projetobarbearia/ProjetoBarbeariaApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabrielrichard.projetobarbearia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBarbeariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBarbeariaApplication.class, args);
+	}
+
+}
