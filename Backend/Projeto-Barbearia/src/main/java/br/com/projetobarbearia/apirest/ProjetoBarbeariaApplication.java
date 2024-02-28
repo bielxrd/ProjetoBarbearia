1cfd40ce80c81fb6141c68cdee55da36a9ca9e64
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-02-28 18:27:58 -0300

    estrutura criada pelo initializr

---
+++ Backend/Projeto-Barbearia/src/main/java/br/com/projetobarbearia/apirest/ProjetoBarbeariaApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetobarbearia.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBarbeariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBarbeariaApplication.class, args);
+	}
+
+}
---
+++ Backend/Projeto-Barbearia/src/test/java/br/com/projetobarbearia/apirest/ProjetoBarbeariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetobarbearia.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBarbeariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
